7bd708de4e77c3f47b7c79b5c187f5868fabd72e
alicendeh <alicendeh16@gmail.com>
2022-02-23 12:40:14 +0100

    Done with deleting users info

--- goodwillfinancial/routes/user.js
+++ goodwillfinancial/routes/user.js
@@ -48,4 +48,37 @@ router.get("/viewAll", async (req, res) => {
 
 //edit user info
 
+router.put("/updateUserInfo/:UserID", async (req, res) => {
+  try {
+    let user = await Users.findByIdAndUpdate(req.params.UserID, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(400).json({ msg: "No such user" });
+    }
+    return res.status(200).json({ user });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+//delete account
+
+router.delete("/deleteUserAcct/:userID", async (req, res) => {
+  try {
+    let admin = await Users.findById(req.params.userID);
+
+    if (!admin) return res.status(404).json({ msg: "admin not found" });
+
+    await Users.findByIdAndRemove(req.params.userID);
+
+    res.json({ msg: "user removed" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
